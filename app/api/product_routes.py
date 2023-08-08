df04ec97cdd4c3eda34e39e96259e9576df1745a
Connie Ooi <connie.ooi18@gmail.com>
2023-08-08 16:42:15 -0700

    delete product route added

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import Product, User
+from app.models import Product, User, db
 from .auth_routes import validation_errors_to_error_messages
 
 product_routes = Blueprint('products', __name__)
@@ -13,9 +13,31 @@ def get_all_products():
     return {"products": [product.to_dict() for product in products]}
 
 
-#get all of the logged in users products
+#get all of the logged in user's products
 @product_routes.route('/user')
 @login_required
 def get_user_products():
     products = Product.query.filter_by(user_id = current_user.id )
     return {"products": [product.to_dict() for product in products]}
+
+
+#get one product's details
+@product_routes.route('/<int:productId>')
+def get_one_product(productId):
+    product = Product.query.get(productId)
+    if not product:
+        return jsonify({"erorr": "Product not found"}), 404
+    response = product.to_dict()
+    return response
+
+
+#delete a product
+@product_routes.route('/<int:productId>', methods=["DELETE"])
+@login_required
+def delete_product(productId):
+    product = Product.query.get(productId)
+    if not product:
+        return jsonify({"erorr": "Product not found"}), 404
+    db.session.delete(product)
+    db.session.commit()
+    return {"id": productId}

54b282db358313d31e08dedb9c3f849eb8a8df3b
Connie Ooi <connie.ooi18@gmail.com>
2023-08-11 11:14:40 -0700

    remove all products from cart done

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -11,7 +11,7 @@ cart_routes = Blueprint('carts', __name__)
 @cart_routes.route('')
 @login_required
 def get_cart():
-    products = ShoppingCart.query.filter_by(user_id = current_user.id)
+    products = ShoppingCart.query.filter_by(user_id = current_user.id).all()
 
     if products:
         product_dict = [product.to_dict() for product in products]
@@ -23,6 +23,21 @@ def get_cart():
         return result
     else:
         return jsonify({"message": "You have no products in your cart"}), 404
+
+
+#remove all products from a user's cart
+@cart_routes.route('', methods=['DELETE'])
+@login_required
+def remove_all_cart():
+    carts = ShoppingCart.query.filter_by(user_id = current_user.id)
+
+    if not carts:
+        return jsonify({"error": "Your cart is empty"}), 404
+
+    for cart in carts:
+        db.session.delete(cart)
+        db.session.commit()
+    return {"message": "Your cart is now empty"}
 
 
 #delete a product from a user's cart

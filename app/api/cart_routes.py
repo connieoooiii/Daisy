0426f4e9ce90a76d7752634eef0e9906b8f59e33
Connie Ooi <connie.ooi18@gmail.com>
2023-08-11 14:05:26 -0700

    get total of shopping cart

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -16,20 +16,25 @@ def get_cart():
     if products:
         product_dict = [product.to_dict() for product in products]
         result = []
+        total = 0
         for product in product_dict:
             item = Product.query.get(product['product_id'])
             return_item = item.to_dict()
             result.append(return_item)
+            # total += return_item['price']
+        # result.append({'total_price': total})
         return result
     else:
         return jsonify({"message": "You have no products in your cart"}), 404
+
+
 
 
 #remove all products from a user's cart
 @cart_routes.route('', methods=['DELETE'])
 @login_required
 def remove_all_cart():
-    carts = ShoppingCart.query.filter_by(user_id = current_user.id)
+    carts = ShoppingCart.query.filter_by(user_id = current_user.id).all()
 
     if not carts:
         return jsonify({"error": "Your cart is empty"}), 404
@@ -38,6 +43,27 @@ def remove_all_cart():
         db.session.delete(cart)
         db.session.commit()
     return {"message": "Your cart is now empty"}
+
+
+#get the total amount of a user's shopping cart
+@cart_routes.route('/total')
+@login_required
+def get_cart_total():
+    products = ShoppingCart.query.filter_by(user_id = current_user.id).all()
+
+    if products:
+        product_dict = [product.to_dict() for product in products]
+        # result = []
+        total = 0
+        for product in product_dict:
+            item = Product.query.get(product['product_id'])
+            return_item = item.to_dict()
+            # result.append(return_item)
+            total += return_item['price']
+        return jsonify({'total_price': total})
+
+    else:
+        return jsonify({'total_price': 0})
 
 
 #delete a product from a user's cart

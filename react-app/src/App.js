248f3b65f026078fd32e3e2f6449dadff941fca3
Connie Ooi <connie.ooi18@gmail.com>
2023-08-14 23:30:32 -0700

    / goes to all products rn

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -23,6 +23,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <ProductsIndex />
+          </Route>
           <Route exact path="/products">
             <ProductsIndex />
           </Route>

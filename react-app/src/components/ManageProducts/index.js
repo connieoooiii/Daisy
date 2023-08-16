e022eb2196e2acf5184200326b26d42526e69586
Connie Ooi <connie.ooi18@gmail.com>
2023-08-16 15:34:01 -0700

    managed pro page

--- react-app/src/components/ManageProducts/index.js
+++ react-app/src/components/ManageProducts/index.js
@@ -8,6 +8,7 @@ import OpenModalButton from "../OpenModalButton";
 import DeleteProduct from "../DeleteProduct";
 import "./ManageProducts.css";
 import UpdateProduct from "../UpdateProduct";
+import CreateProduct from "../CreateProduct";
 
 export default function ManageProducts() {
   const dispatch = useDispatch();
@@ -23,13 +24,32 @@ export default function ManageProducts() {
     dispatch(getUserProductsThunk());
   }, [dispatch]);
 
-  if (!products) return null;
+  if (products.length < 1) {
+    return (
+      <div className="manage-wrap">
+        <img
+          src="https://cdn.discordapp.com/attachments/1138505164358164483/1141497587921330276/daisy-.jpeg"
+          alt="daisy"
+          className="daisy-manage"
+        />
+        <div className="man-div">
+          <div className="create-first">Create your first product!</div>
+          <div className="man-create">
+            <OpenModalButton
+              modalComponent={<CreateProduct />}
+              buttonText="Create A Product"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <div className="product-card-wrap">
+      <div className="manage-card-wrap">
         {products.map((product) => (
-          <div key={product.id}>
+          <div key={product.id} className="edit-pro">
             <ProductCard product={product} />
             <div className="the-buttons">
               <div className="editpro-wrap">
